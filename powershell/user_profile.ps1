14193b94b24bd538cf7e3d145fece30bb3b54bbc
eriknas <987ww765@rambler.ru>
2021-12-22 10:44:58 +0300

    add powershell config

---
+++ powershell/user_profile.ps1
@@ -0,0 +1,11 @@
+# Prompt
+Import-Module posh-git
+Import-Module oh-my-posh
+Import-Module -Name Terminal-Icons
+
+Set-PoshPrompt Paradox
+
+# Alias
+Set-Alias vim nvim
+Set-Alias apt scoop
+Set-Alias grep findstr
